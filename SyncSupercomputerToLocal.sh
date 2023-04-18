3a2f65e9fac1288be0dacbb269a367342cb0f5ff
Jesse <jesseclarkwins@gmail.com>
2023-04-17 21:07:59 -0600

    Added script to sync files

---
+++ SyncSupercomputerToLocal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set the source and destination directories
+SRC_DIR="/home/jclar234/TrOCR"
+DEST_DIR="/home/jesse/TrOCR/ZZZ"
+
+# Set the username and IP address of the remote computer
+REMOTE_USER="jclar234"
+REMOTE_IP="ssh.rc.byu.edu"
+
+# Use rsync to sync the files between the two directories
+rsync -avz --delete -e ssh ${SRC_DIR} ${REMOTE_USER}@${REMOTE_IP}:${DEST_DIR}
